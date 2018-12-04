18e6cf12bae5bc24f60628de774167f9ec197bb6
Wyatt Lynch (RIT Student) <wal8273@ad.rit.edu>
2018-12-04 15:21:00 -0500

    edited content

--- fall2018sample/fall2018sample.js
+++ fall2018sample/fall2018sample.js
@@ -1,1 +1,8 @@
 /* Don't use <script> tags in a linked js file! */
+$(".menuitem").next().hide(0);
+$(".menuitem").click(function()
+        {
+            $(".menuitem").next().toggle("fast");// can use keywords slow and fast to direct timing
+        });
+
+        
