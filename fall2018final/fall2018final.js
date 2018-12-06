b8f7c701ac216baab64cd62dce13dcc4295392b4
Wyatt Lynch (RIT Student) <wal8273@ad.rit.edu>
2018-12-06 15:13:40 -0500

    added stuff

---
+++ fall2018final/fall2018final.js
@@ -0,0 +1,14 @@
+$(".menu").next(".items").hide(0);
+$(".menu").click(function()
+        {
+            $(this).next(".items").slideToggle("fast");// can use keywords slow and fast to direct timing
+        });
+
+        
+        let content = ("content1.txt");  // sets default content
+        $("#choose-content").val(content); // changes menu option to default
+        $("#content").load(content);   // retrieves only default element
+        $("#choose-content").change(function() {//sets up on change event
+            content = $(this).val();//grabs current value
+            $("#content").load(content); //loads current menu val
+        
