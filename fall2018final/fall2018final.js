f9dde067caf344f97c8542c73f79f167e19bebb0
Wyatt Lynch (RIT Student) <wal8273@ad.rit.edu>
2018-12-06 15:16:40 -0500

    final attempt

--- fall2018final/fall2018final.js
+++ fall2018final/fall2018final.js
@@ -4,11 +4,11 @@ $(".menu").click(function()
             $(this).next(".items").slideToggle("fast");// can use keywords slow and fast to direct timing
         });
 
-        
-        let content = ("content1.txt");  // sets default content
-        $("#choose-content").val(content); // changes menu option to default
-        $("#content").load(content);   // retrieves only default element
-        $("#choose-content").change(function() {//sets up on change event
-            content = $(this).val();//grabs current value
-            $("#content").load(content); //loads current menu val
+        //couldn't figure out AJAX but broke menus when left in
+        //let content = ("article0.txt");  // sets default content
+        //$("aside h2").val(content + " #h2"); // changes menu option to default
+        //$("h2").load(content);   // retrieves only default element
+       // $("input[name=article]").change(function() {//sets up on change event
+           // content = $(this).val();//grabs current value
+            //$("aside h2").load(content); //loads current menu val
         
